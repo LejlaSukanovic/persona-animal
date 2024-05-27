def6fd00945b2429a73cf469f242b5a1a22e4a13
Katarina Srdanov <katarina.srdanov@student.um.si>
2024-05-27 10:49:16 +0200

    popravek ux

--- public/pregledOcenitve.js
+++ public/pregledOcenitve.js
@@ -1,5 +1,4 @@
 document.querySelector('.close-btn').addEventListener('click', function() {
-    this.closest('.card').style.display = 'none';
     fetch(`/samoocenitev/brisanje`, {
                   method: 'GET',
               });
