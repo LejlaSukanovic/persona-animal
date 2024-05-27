b5563939aaf2dc935f7d93a468284918c694c9e9
Lejla Sukanović <lejla.sukanovic@student.um.si>
2024-05-27 10:07:33 +0200

    pregled ocenitve front end almost done

---
+++ public/pregledOcenitve.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const infoContainer = document.querySelector('.info-container');
+    
+
+    let startY, startTop;
+  
+    infoContainer.addEventListener('mousedown', function(e) {
+        startY = e.clientY;
+        startTop = parseInt(window.getComputedStyle(this).top, 10);
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mouseup', onMouseUp);
+      });
+      
+  
+      function onMouseMove(e) {
+        const dy = e.clientY - startY;
+        const newTop = Math.min(Math.max(150, startTop + dy), 300);
+        infoContainer.style.top = `${newTop}px`;
+    
+        // Calculate how much of the image is revealed, adjusting the brightness accordingly
+        const percentRevealed = (newTop - 150) / 150;
+        const filterValue = 100 - ((1 - percentRevealed) * 50); // Reversed the effect
+        document.querySelector('.image-container img').style.filter = `brightness(${filterValue}%)`;
+    }
+    
+    
+      
+  
+      function onMouseUp() {
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+      }
+  });
+  
